f93a0efc41c6f4bfdb0de26eb694539a91a14552
Deependra-Das <iamdeep75@gmail.com>
2024-06-25 02:04:40 +0530

    Added Gameplay Service
    - Added the header & source files for the Gameplay Service with View & Controller
    - Added the namespace, classes with respective data members & functions to render background
    - Updated the Service Locator to access the Gameplay Service & render the background when gamestate is Gameplay

---
+++ Space-Invaders/Header/Gameplay/GameplayService.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../../Header/Gameplay/GameplayController.h"
+
+namespace Gameplay
+{
+	class GameplayService
+	{
+	private:
+		GameplayController* gameplay_Controller;
+
+	public:
+		GameplayService();
+		~GameplayService();
+
+		void initialize();
+		void update();
+		void render();
+		
+	};
+}
--- Space-Invaders/Header/Global/ServiceLocator.h
+++ Space-Invaders/Header/Global/ServiceLocator.h
@@ -6,6 +6,7 @@
 #include "../../Header/Time/TimeService.h"
 #include "../../Header/UI/UIService.h"
 #include "../../Header/Enemy/EnemyService.h"
+#include "../../Header/Gameplay/GameplayService.h"
 
 namespace Global
 {
@@ -24,6 +25,7 @@ namespace Global
         Player::PlayerService* player_service;
         UI::UIService* ui_service;
         Enemy::EnemyService* enemy_service;
+        Gameplay::GameplayService* gameplay_service;
 
         // Private Methods:
         void createServices(); 
@@ -44,5 +46,6 @@ namespace Global
         Time::TimeService* getTimeService();
         UI::UIService* getUIService();
         Enemy::EnemyService* getEnemyService();
+        Gameplay::GameplayService* getGameplayService();
     };
 }
