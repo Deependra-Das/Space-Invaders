7a7282435a5b9a2bb424727270ba32dcbfff3a93
Deependra-Das <iamdeep75@gmail.com>
2024-06-30 16:24:39 +0530

    Added Bullet Service
    - Updated the Bullet Service to hold bullets in a vector list & use functions to render & spawn based on various type of controllers
    - Updated the Service Locator to access the Bullet Service

--- Space-Invaders/Header/Bullet/BulletService.h
+++ Space-Invaders/Header/Bullet/BulletService.h
@@ -1,16 +1,33 @@
 #pragma once
+#include <vector>
+#include "SFML/System/Vector2.hpp"
+#include "../../Header/Projectile/IProjectile.h"
 
 namespace Bullet
 {
+	enum class BulletType;
+	enum class MovementDirection;
+	class BulletController;
+
 	class BulletService
 	{
 	private:
+		std::vector<Projectile::IProjectile*> bullet_list;
+
+		BulletController* createBullet(BulletType type);
+
+		void destroy();
+
+	public:
+		BulletService();
+		virtual ~BulletService();
+
 		void initialize();
 		void update();
 		void render();
 
-	public:
-		
+		BulletController* spawnBullets(BulletType bullet_type , sf::Vector2f position, MovementDirection direction);
+		void destroyBullet(BulletController* bullet_controller);
 
 	};
 }
