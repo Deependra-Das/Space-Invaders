6c8bf4a2b9b0fe2692dd275f5ee173a6cf10c954
Deependra-Das <iamdeep75@gmail.com>
2024-06-30 15:30:01 +0530

    Updated Bullet Model & View
    - Updated the BulletModel & BulletView to hold the data for the Bullets & then  render the sprites with respective member functions

--- Space-Invaders/Header/Bullet/BulletModel.h
+++ Space-Invaders/Header/Bullet/BulletModel.h
@@ -1,1 +1,37 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+
+namespace Bullet
+{
+	enum class MovementDirection;
+	enum class BulletType;
+
+	class BulletModel
+	{
+	private:
+		MovementDirection movement_direction;
+		sf::Vector2f bullet_position;
+		BulletType bullet_type;
+		float movement_speed=300.0f;
+
+	public:
+		BulletModel(BulletType type);
+		~BulletModel();
+
+		void initialize(sf::Vector2f position, MovementDirection direction);
+
+		MovementDirection getMovementDirection();
+		void setMovementDirection(MovementDirection direction);
+
+		sf::Vector2f getBulletPosition();
+		void setBulletPosition(sf::Vector2f position);
+
+		BulletType getBulletType();
+		void setBulletType(BulletType type);
+
+		float getMovementSpeed();
+		void setMovementSpeed(float speed);
+
+	};
+}
---
+++ Space-Invaders/Source/Bullet/BulletModel.cpp
@@ -0,0 +1,60 @@
+#include "../../Header/Bullet/BulletModel.h"
+
+namespace Bullet
+{
+	BulletModel::BulletModel(BulletType type)
+	{
+		bullet_type = type;
+	}
+
+	BulletModel::~BulletModel()
+	{
+
+	}
+
+	void BulletModel::initialize(sf::Vector2f position, MovementDirection direction)
+	{
+		bullet_position = position;
+		movement_direction = direction;
+	}
+
+	MovementDirection BulletModel::getMovementDirection()
+	{
+		return movement_direction;
+	}
+
+	void BulletModel::setMovementDirection(MovementDirection direction)
+	{
+		movement_direction = direction;
+	}
+
+	sf::Vector2f BulletModel::getBulletPosition()
+	{
+		return bullet_position;
+	}
+
+	void BulletModel::setBulletPosition(sf::Vector2f position)
+	{
+		bullet_position = position;
+	}
+
+	BulletType BulletModel::getBulletType()
+	{
+		return bullet_type;
+	}
+
+	void BulletModel::setBulletType(BulletType type)
+	{
+		bullet_type = type;
+	}
+
+	float BulletModel::getMovementSpeed()
+	{
+		return movement_speed;
+	}
+
+	void BulletModel::setMovementSpeed(float speed)
+	{
+		movement_speed = speed;
+	}
+}
