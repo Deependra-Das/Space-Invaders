6435403f828bc720b8dfddd4551ae8a63131c1e4
Deependra-Das <iamdeep75@gmail.com>
2024-07-02 12:27:02 +0530

    Updated UI Service to use Interface
    - Updated the UI Service to include UI Interface as dependency to communicate with Main menu  Controller

--- Space-Invaders/Source/UI/UIService.cpp
+++ Space-Invaders/Source/UI/UIService.cpp
@@ -5,6 +5,7 @@ namespace UI
 {
 	using namespace Main;
 	using namespace MainMenu;
+	using namespace Interface;
 
 	UIService::UIService()
 	{
@@ -18,39 +19,62 @@ namespace UI
 		destroyControllers();
 	}
 
-	void UIService::initialize()
-	{
-		initializeControllers();
-	}
-
-	void UIService::update()
-	{
-		switch (GameService::getGameState())
-		{
-			case GameState::MAIN_MENU:
-			return main_menu_controller->update();;
-			break;
-		}
-	}
-
-	void UIService::render()
-	{
-		switch (GameService::getGameState())
-		{
-			case GameState::MAIN_MENU:
-			return main_menu_controller->render();
-			break;
-		}
-	}
-
 	void UIService::createControllers()
 	{
 		main_menu_controller = new MainMenuUIController();
 	}
 
+	void UIService::initialize()
+	{
+		initializeControllers();
+	}
+
 	void UIService::initializeControllers()
 	{
 		main_menu_controller->initialize();
+	}
+
+	void UIService::update()
+	{
+		IUIController* ui_controller = getCurrentUIController();
+
+		if (ui_controller)
+		{
+			ui_controller->update();
+		}
+			
+	}
+
+	void UIService::render()
+	{
+		IUIController* ui_controller = getCurrentUIController();
+
+		if (ui_controller)
+		{
+			ui_controller->render();
+		}
+	}
+
+	void UIService::showScreen()
+	{
+		IUIController* ui_controller = getCurrentUIController();
+
+		if (ui_controller)
+		{
+			ui_controller->show();
+		}
+	}
+
+	IUIController* UIService::getCurrentUIController()
+	{
+		switch (GameService::getGameState())
+		{
+		case GameState::MAIN_MENU:
+			return main_menu_controller;
+
+		default:
+			return nullptr;
+		}
 	}
 
 	void UIService::destroyControllers()
