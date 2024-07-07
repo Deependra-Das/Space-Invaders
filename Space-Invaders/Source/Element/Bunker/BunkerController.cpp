8f41dd548dab9de9f8d3017c4c916cc8d85db46f
Deependra-Das <iamdeep75@gmail.com>
2024-07-08 00:33:16 +0530

    Implemented Bunker Collision

--- Space-Invaders/Source/Element/Bunker/BunkerController.cpp
+++ Space-Invaders/Source/Element/Bunker/BunkerController.cpp
@@ -1,11 +1,18 @@
 #include "../../Header/Element/Bunker/BunkerController.h"
 #include "../../Header/Element/Bunker/BunkerView.h"
+#include "../../../Header/Bullet/BulletController.h"
+#include "../../../Header/Entity/EntityConfig.h"
+#include "../../Header/Global/ServiceLocator.h"
+#include "../../../Header/Bullet/BulletConfig.h"
 
 namespace Element
 {
-
 	namespace Bunker
 	{
+		using namespace Bullet;
+		using namespace Entity;
+		using namespace Global;
+
 		BunkerController::BunkerController()
 		{
 			bunker_view=new BunkerView();
@@ -36,6 +43,22 @@ namespace Element
 		{
 			return bunker_data.position;;
 		}
+
+		void BunkerController::onCollision(ICollider* other_collider)
+		{
+			BulletController* bullet_controller = dynamic_cast<BulletController*>(other_collider);
+
+			if (bullet_controller && bullet_controller->getBulletType() == BulletType::TORPEDO)
+			{
+				ServiceLocator::getInstance()->getElementService()->destroyBunker(this);
+			}
+		}
+
+		const sf::Sprite& BunkerController::getColliderSprite() 
+		{ 
+			return bunker_view->getBunkerSprite(); 
+		}
+
 	}
 
 }
